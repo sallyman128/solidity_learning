78ad3629404be19f437bccb68d0f8bdf62078c81
sallyman128 <salmaan.arif.ali@gmail.com>
2023-12-01 23:13:54 -0500

    refactored tweets to be array

--- contracts/Twitter.sol
+++ contracts/Twitter.sol
@@ -3,13 +3,17 @@
 pragma solidity ^0.8.22;
 
 contract Twitter {
-    mapping(address => string) public tweets;
+    mapping(address => string[]) public tweets;
 
     function createTweet(string memory _tweet) public {
-        tweets[msg.sender] = _tweet;
+        tweets[msg.sender].push(_tweet);
     }
 
-    function getTweet(address _owner) public view returns(string memory) {
+    function getTweet(address _owner, uint _i) public view returns(string memory) {
+        return tweets[_owner][_i];
+    }
+
+    function getAllTweets(address _owner) public view returns(string[] memory) {
         return tweets[_owner];
     }
 }
