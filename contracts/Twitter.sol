a1d9ac8c46eacbe9c8453cd39b6e470540560887
sallyman128 <salmaan.arif.ali@gmail.com>
2023-12-01 23:53:12 -0500

    owner and only owner can change max tweet length

--- contracts/Twitter.sol
+++ contracts/Twitter.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.22;
 
 contract Twitter {
 
-    uint16 constant MAX_TWEET_LENGTH = 280;
+    uint16 MAX_TWEET_LENGTH = 280;
 
     struct Tweet {
         address author;
@@ -14,6 +14,20 @@ contract Twitter {
     }
 
     mapping(address => Tweet[]) tweets;
+    address owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "You are not the owner!");
+        _;
+    }
+
+    function changeTweetLength(uint16 newTweetLength) public onlyOwner {
+        MAX_TWEET_LENGTH = newTweetLength;
+    }
 
     function createTweet(string memory _tweet) public {
         require(bytes(_tweet).length <= MAX_TWEET_LENGTH, "Tweet is too long.");
